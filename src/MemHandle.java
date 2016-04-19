59a880aca45be397572973e5e361bff1fc6e5e38
Jonathan <jondef95@vt.edu>
2016-04-19 14:57:38 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/MemHandle.java changed at Tue Apr 19 14:57:37 EDT 2016

--- src/MemHandle.java
+++ src/MemHandle.java
@@ -1,4 +1,14 @@
+/**
+ * class that will act as a receipt from the Memory Manager.
+ * @author Preston Lattimer (platt) Jonathan DeFreeuw (jondef95)
+ * @version 1
+ *
+ */
+public class MemHandle 
+{
 
-public class MemHandle {
-
+    public MemHandle()
+    {
+        return;
+    }
 }
