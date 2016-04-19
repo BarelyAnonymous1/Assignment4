931ae3078eb3717d46bb236b0b0abf3a4a1545f7
Jonathan <jondef95@vt.edu>
2016-04-19 14:59:40 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/RectangleDiskTest.java changed at Tue Apr 19 14:59:40 EDT 2016

--- src/RectangleDiskTest.java
+++ src/RectangleDiskTest.java
@@ -6,15 +6,16 @@ import student.TestCase;
  * @author CS 3114 Staff
  * @version April 13, 2016
  */
-public class RectangleDiskTest extends TestCase {
+public class RectangleDiskTest extends TestCase
+{
 
     /**
      * list of inputs for first test
      */
     String[] input1;
-    
+
     /**
-     *  list of inputs for second test
+     * list of inputs for second test
      */
     String[] input2;
 
@@ -31,31 +32,32 @@ public class RectangleDiskTest extends TestCase {
     }
 
     /**
-     * This method gets you credit for testing a bad
-     * set of parameters and for initializing the
-     * Driver class.
+     * This method gets you credit for testing a bad set of parameters and for
+     * initializing the Driver class.
      */
-    public void testBadParams() {
+    public void testBadParams()
+    {
         RectangleDisk rectDisk = new RectangleDisk();
         assertNotNull(rectDisk);
         String[] params = { "bad", "params" };
         RectangleDisk.main(params);
         assertFuzzyEquals(
-            "Usage: RectangleDisk <commandfile> "
-            + "<diskFile> <numBuffs> <buffSize>",
-            systemOut().getHistory());
+                "Usage: RectangleDisk <commandfile> "
+                        + "<diskFile> <numBuffs> <buffSize>",
+                systemOut().getHistory());
     }
 
     /**
-     * This method gets you credit for testing a good
-     * set of parameters.
+     * This method gets you credit for testing a good set of parameters.
      */
-    public void testGoodParams() {
+    public void testGoodParams()
+    {
         String[] params = { "commands.txt", "dataFile.dat", "5", "4096" };
         RectangleDisk.main(params);
-        assertFuzzyEquals("Found expected parameter list.", 
-            systemOut().getHistory());
+        assertFuzzyEquals("Found expected parameter list.",
+                systemOut().getHistory());
     }
+
     /**
      * This method is simply to get code coverage of the class declaration.
      */
@@ -64,8 +66,9 @@ public class RectangleDiskTest extends TestCase {
         RectangleDisk dum = new RectangleDisk();
         assertNotNull(dum);
         RectangleDisk.main(input1);
-        assertFuzzyEquals("Usage: RectangleDisk <commandfile> "
-                + "<diskFile> <numBuffs> <buffSize>\n",
+        assertFuzzyEquals(
+                "Usage: RectangleDisk <commandfile> "
+                        + "<diskFile> <numBuffs> <buffSize>\n",
                 systemOut().getHistory());
     }
 
@@ -82,7 +85,7 @@ public class RectangleDiskTest extends TestCase {
      */
     public void testSearchFile()
     {
-        String[] input = { "SyntaxTest.txt", "dataFile.dat", "5", "4096"};
+        String[] input = { "SyntaxTest.txt", "dataFile.dat", "5", "4096" };
         RectangleDisk.main(input);
     }
 }
