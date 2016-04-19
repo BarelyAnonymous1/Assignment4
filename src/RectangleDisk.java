af79f382b912c3044b1f56388127b931b6753275
Jonathan <jondef95@vt.edu>
2016-04-19 14:13:25 -0400

    stubs are beginning to get setup will test in a moment

--- src/RectangleDisk.java
+++ src/RectangleDisk.java
@@ -1,29 +1,42 @@
+import java.io.IOException;
+
 /**
  * { your description of the project here }
  */
 
 /**
- * The class containing the main method, the entry point
- * of the application.
+ * The class containing the main method, the entry point of the application.
  *
  * @author { your name here }
  * @version { put something here }
  */
-public class RectangleDisk {
+public class RectangleDisk
+{
 
     /**
      * The entry point for the application.
      *
-     * @param args The command line arguments.
+     * @param args
+     *            The command line arguments.
      */
-    public static void main(String[] args) {
-        if (args.length != 4) {
+    public static void main(String[] args)
+    {
+        if (args.length != 4)
+        {
             System.out.println("Usage: RectangleDisk <commandfile> "
-                + "<diskFile> <numBuffs> <buffSize>");
+                    + "<diskFile> <numBuffs> <buffSize>");
         }
-        else {
-            CommandParser parser = new CommandParser(args[0]);
-            parser.parseFile();
+        else
+        {
+            try
+            {
+                CommandParser parser = new CommandParser(args[0]);
+                parser.parseFile();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
         }
     }
 }
