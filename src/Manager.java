8e36e575922b3cdcf042aaad5ed331ffd595c5cc
Jonathan <jondef95@vt.edu>
2016-04-24 21:38:20 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/Manager.java changed at Sun Apr 24 21:38:19 EDT 2016

--- src/Manager.java
+++ src/Manager.java
@@ -141,6 +141,7 @@ public class Manager
         ByteBuffer buffer = ByteBuffer.allocate(messageSize);
         buffer.putShort((short) newMessage.length);
         System.arraycopy(buffer.array(), 0, tempDisk, h, messageSize);
+        System.out.println(newMessage.length);
         System.arraycopy(newMessage, 0, tempDisk, h + 2,
                 newMessage.length);
     }
