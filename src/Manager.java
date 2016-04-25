2b3f9eb2b4c40ba49d1d85e43f8198dba0aea30f
jondef95@vt.edu <jondef95@github.com>
2016-04-25 18:59:13 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/Manager.java changed at Mon Apr 25 18:59:12 EDT 2016

--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,4 @@
 import java.nio.ByteBuffer;
-import java.util.*;
 
 /**
  * Memory manager class that will communicate with a buffer pool to keep a
@@ -53,6 +52,7 @@ public class Manager
     {
         instance = null;
     }
+
     /**
      * sets the size of a block of the freelist
      * 
@@ -62,7 +62,7 @@ public class Manager
     public void setSize(int sz)
     {
         blockSize = sz;
-        tempDisk = new byte[10*blockSize];
+        tempDisk = new byte[10 * blockSize];
         freeList.insert(new DoublyLinkedNode(0, blockSize));
         numBlocks++;
     }
@@ -130,8 +130,8 @@ public class Manager
     {
         System.arraycopy(tempDisk, h, sizeArr, 0, messageSize);
         short sizeNum = ByteBuffer.wrap(sizeArr).getShort();
-        byte[] temp = new byte[messageSize+sizeNum];
-        System.arraycopy(tempDisk, h+messageSize, temp, 0, temp.length);
+        byte[] temp = new byte[messageSize + sizeNum];
+        System.arraycopy(tempDisk, h + messageSize, temp, 0, temp.length);
         return temp;
     }
 
