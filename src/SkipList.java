5075145ca83f55de20f0f1d6c6ac796858e845db
Jonathan <jondef95@vt.edu>
2016-04-25 02:39:50 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/SkipList.java changed at Mon Apr 25 02:39:50 EDT 2016

--- src/SkipList.java
+++ src/SkipList.java
@@ -138,6 +138,7 @@ public class SkipList<K extends Comparable<K>, E>
         for (int i = 0; i <= newLevel; i++)
         {
             update[i].next[i] = currPos;
+            System.out.println("new update: " + update[i].next[i]);
             Manager.getInstance().replaceRecord(updateHandles[i],
                     Serializer.serialize(update[i]));
         }
