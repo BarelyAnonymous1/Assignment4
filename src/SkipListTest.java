f00fc9eaaa7bd2dba7b0e219d803b3c85bc52d37
Jonathan <jondef95@vt.edu>
2016-04-25 01:43:30 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/SkipListTest.java changed at Mon Apr 25 01:43:30 EDT 2016

--- src/SkipListTest.java
+++ src/SkipListTest.java
@@ -34,8 +34,8 @@ public class SkipListTest extends TestCase
      */
     public void testInsert() throws Exception
     {
-        list.insert(pair2);
         list.insert(pair1);
+        list.insert(pair2);
 //        assertNull(list.search("node3"));
         list.insert(pair3);
 //        assertEquals(pair3.compareTo(list.search("node3").getPair()), 0);
