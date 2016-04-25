cfa5fb73cda9dc3d2af209ddb35addd56b981a30
Jonathan <jondef95@vt.edu>
2016-04-25 02:10:37 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/SkipListTest.java changed at Mon Apr 25 02:10:36 EDT 2016

--- src/SkipListTest.java
+++ src/SkipListTest.java
@@ -36,63 +36,63 @@ public class SkipListTest extends TestCase
     {
         list.insert(pair1);
         list.insert(pair2);
-//        assertNull(list.search("node3"));
+        // assertNull(list.search("node3"));
         list.insert(pair3);
         list.insert(new KVPair<String, Integer>("node4", 4));
-//        assertEquals(pair3.compareTo(list.search("node3").getPair()), 0);
+        // assertEquals(pair3.compareTo(list.search("node3").getPair()), 0);
 
         list.dump();
     }
 }
 
-//    /**
-//     * make sure that the list will adjust the head when a node is added higher
-//     * than the head
-//     */
-//    public void testRandomInsert() throws Exception
-//    {
-//        TestableRandom.setNextBooleans(false, true, true, false);
-//        assertEquals(1, list.getHead().getLevel());
-//        assertTrue(list.insert(pair1));
-//        assertEquals(1, list.getHead().getLevel());
-//        assertTrue(list.insert(pair2));
-//        assertEquals(2, list.getHead().getLevel());
-//        list.dump();
-//    }
+// /**
+// * make sure that the list will adjust the head when a node is added higher
+// * than the head
+// */
+// public void testRandomInsert() throws Exception
+// {
+// TestableRandom.setNextBooleans(false, true, true, false);
+// assertEquals(1, list.getHead().getLevel());
+// assertTrue(list.insert(pair1));
+// assertEquals(1, list.getHead().getLevel());
+// assertTrue(list.insert(pair2));
+// assertEquals(2, list.getHead().getLevel());
+// list.dump();
+// }
 //
-//    /**
-//     * creates a fake region to check for new rectangles
-//     */
-//    public void testRegionSearch() throws Exception
-//    {
-//        Rectangle region = new Rectangle("region", 100, 100, 200, 200);
-//        SkipList<String, Rectangle> regionList = 
-//                new SkipList<String, Rectangle>();
-//        regionList.insert(new KVPair<String, Rectangle>("notIntersect",
-//                new Rectangle("notIntersect", 10, 10, 20, 20)));
-//        assertFalse(regionList.regionSearch(region));
-//        regionList.insert(new KVPair<String, Rectangle>("intersect1",
-//                new Rectangle("intersect1", 75, 75, 200, 250)));
-//        assertTrue(regionList.regionSearch(region));
-//    }
+// /**
+// * creates a fake region to check for new rectangles
+// */
+// public void testRegionSearch() throws Exception
+// {
+// Rectangle region = new Rectangle("region", 100, 100, 200, 200);
+// SkipList<String, Rectangle> regionList =
+// new SkipList<String, Rectangle>();
+// regionList.insert(new KVPair<String, Rectangle>("notIntersect",
+// new Rectangle("notIntersect", 10, 10, 20, 20)));
+// assertFalse(regionList.regionSearch(region));
+// regionList.insert(new KVPair<String, Rectangle>("intersect1",
+// new Rectangle("intersect1", 75, 75, 200, 250)));
+// assertTrue(regionList.regionSearch(region));
+// }
 //
-//    /**
-//     * tests that the intersections test successfully finds an intersection and
-//     * returns the appropriate boolean
-//     */
-//    public void testIntersections() throws Exception
-//    {
-//        SkipList<String, Rectangle> intersectList = 
-//                new SkipList<String, Rectangle>();
-//        intersectList.insert(new KVPair<String, Rectangle>("notIntersect",
-//                new Rectangle("notIntersect", 10, 10, 20, 20)));
-//        assertFalse(intersectList.intersections());
-//        intersectList.insert(new KVPair<String, Rectangle>("intersect1",
-//                new Rectangle("intersect1", 75, 75, 200, 250)));
-//        assertFalse(intersectList.intersections());
-//        intersectList.insert(new KVPair<String, Rectangle>("intersect2",
-//                new Rectangle("intersect2", 100, 100, 200, 200)));
-//        assertTrue(intersectList.intersections());
+// /**
+// * tests that the intersections test successfully finds an intersection and
+// * returns the appropriate boolean
+// */
+// public void testIntersections() throws Exception
+// {
+// SkipList<String, Rectangle> intersectList =
+// new SkipList<String, Rectangle>();
+// intersectList.insert(new KVPair<String, Rectangle>("notIntersect",
+// new Rectangle("notIntersect", 10, 10, 20, 20)));
+// assertFalse(intersectList.intersections());
+// intersectList.insert(new KVPair<String, Rectangle>("intersect1",
+// new Rectangle("intersect1", 75, 75, 200, 250)));
+// assertFalse(intersectList.intersections());
+// intersectList.insert(new KVPair<String, Rectangle>("intersect2",
+// new Rectangle("intersect2", 100, 100, 200, 200)));
+// assertTrue(intersectList.intersections());
 //
-//    }
-//}
+// }
+// }
