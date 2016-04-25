d3296212ed7117997c6cb3fe13b22c88465b1359
Jonathan <jondef95@vt.edu>
2016-04-25 00:04:02 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/SkipNode.java changed at Mon Apr 25 00:04:01 EDT 2016

--- src/SkipNode.java
+++ src/SkipNode.java
@@ -77,6 +77,8 @@ public class SkipNode<K extends Comparable<K>, E>
     @SuppressWarnings("unchecked")
     public E getValue() throws Exception
     {
+        if (pair == -1)
+            return null;
         byte[] obj = Manager.getInstance().getRecord(pair);
         KVPair<K, E> found = ((KVPair<K, E>) Serializer.deserialize(obj));
         if (found != null)
