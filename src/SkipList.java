9020b6e1b946ab366ae9c3f36231fd2bd0d46eda
Jonathan <jondef95@vt.edu>
2016-04-25 00:29:16 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/SkipList.java changed at Mon Apr 25 00:29:16 EDT 2016

--- src/SkipList.java
+++ src/SkipList.java
@@ -66,6 +66,8 @@ public class SkipList<K extends Comparable<K>, E>
         SkipNode<K, E> newHead = new SkipNode<K, E>(-1, newLevel);
         for (int i = 0; i <= level; i++)
         {
+            System.out.println(oldHead.next[i]);
+            System.out.println(newHead.next[i]);
             newHead.next[i] = oldHead.next[i];
         }
         level = newLevel;
