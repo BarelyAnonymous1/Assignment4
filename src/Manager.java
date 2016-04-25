7b00186b3b5441eb2376224d3f538a69541bb844
Jonathan <jondef95@vt.edu>
2016-04-24 23:25:53 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/Manager.java changed at Sun Apr 24 23:25:53 EDT 2016

--- src/Manager.java
+++ src/Manager.java
@@ -95,6 +95,7 @@ public class Manager
         buffer.putShort((short) data.length);
         System.arraycopy(buffer.array(), 0, tempDisk, handle, messageSize);
         System.arraycopy(data, 0, tempDisk, handle + 2, data.length);
+        System.out.println("Handle is: " + handle);
         return handle;
     }
 
