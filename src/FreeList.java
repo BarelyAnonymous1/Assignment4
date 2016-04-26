544fcad70899e5a2e3e9ccc93535934f258093ec
Jonathan <jondef95@vt.edu>
2016-04-26 17:34:01 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/FreeList.java changed at Tue Apr 26 17:34:01 EDT 2016

--- src/FreeList.java
+++ src/FreeList.java
@@ -153,7 +153,7 @@ public class FreeList
         boolean removed = false;
         while (curr != tail)
         {
-            if (curr.prev.index + curr.prev.length == curr.index)
+            if (curr.index + curr.length == curr..next.index)
             {
                 curr.prev.length += curr.length;
                 curr = curr.next;
