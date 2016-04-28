d079889f9ee14c3acec657e9bffbff2e0a4671a4
Jonathan <jondef95@vt.edu>
2016-04-27 23:45:12 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/Manager.java changed at Wed Apr 27 23:45:12 EDT 2016

--- src/Manager.java
+++ src/Manager.java
@@ -140,8 +140,8 @@ public abstract class Manager
         System.arraycopy(pool.getRecord(h, messageSize, diskFile), 0,
             sizeArr, 0, messageSize);
         short sizeNum = ByteBuffer.wrap(sizeArr).getShort();
+        System.out.println(sizeNum);
         return pool.getRecord(h, sizeNum + messageSize, diskFile);
-        return temp;
     }
 
     /**
