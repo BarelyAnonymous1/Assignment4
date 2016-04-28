b1ae19fe6bd3218a6680ce774ea77bda1ee05f54
Jonathan <jondef95@vt.edu>
2016-04-28 00:29:32 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/Manager.java changed at Thu Apr 28 00:29:32 EDT 2016

--- src/Manager.java
+++ src/Manager.java
@@ -101,7 +101,6 @@ public abstract class Manager
         // System.arraycopy(data, 0, tempDisk, handle + messageSize,
         // data.length);
 
-        System.out.println("writing to handle: " + handle);
         pool.writeRecord(handle, messageSize, buffer.array(),
             diskFile);
         pool.writeRecord(handle + messageSize, data.length, data,
