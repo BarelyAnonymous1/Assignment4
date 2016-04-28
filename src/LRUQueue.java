a9ae5aa64aa9172cdeac52b74c957492bede0f74
Jonathan <jondef95@vt.edu>
2016-04-28 14:51:11 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/LRUQueue.java changed at Thu Apr 28 14:51:11 EDT 2016

--- src/LRUQueue.java
+++ src/LRUQueue.java
@@ -40,17 +40,18 @@ public class LRUQueue
      * @param searchFile
      *            the file to search
      */
-    public void makeMostRecent(int recordPos, RandomAccessFile searchFile)
-        throws IOException
+    public void makeMostRecent(int recordPos,
+        RandomAccessFile searchFile) throws IOException
     {
-        DoublyLinkedNode foundNode = list.remove(recordPos
-                / BufferPool.bufferSize, searchFile);
+        DoublyLinkedNode foundNode = list
+            .remove(recordPos / BufferPool.bufferSize, searchFile);
         if (foundNode == null)
         {
             if (list.getSize() < maxSize)
             {
-                list.enqueue(new DoublyLinkedNode((new Buffer(recordPos
-                        / BufferPool.bufferSize, BufferPool.bufferSize, searchFile))));
+                list.enqueue(new DoublyLinkedNode(
+                    (new Buffer(recordPos / BufferPool.bufferSize,
+                        BufferPool.bufferSize, searchFile))));
             }
             else
             {
