da68d0d6d26bda3ba348bf12f4a4dc8c9fba0960
Jonathan <jondef95@vt.edu>
2016-04-28 01:02:15 -0400

    COPY PASTE ERRORS WILL BE THE DEATH OF ME

--- src/CommandParserTest.java
+++ src/CommandParserTest.java
@@ -11,18 +11,18 @@ public class CommandParserTest extends TestCase
 {
     private CommandParser parser;
 
-//    /**
-//     * tests to see if the parser will correctly handle an incorrect file
-//     */
-//    public void testSearchFail() throws Exception
-//    {
-//        String[] args = {"fail", "1", "2", "512"};
-//        parser = new CommandParser(args);
-//        boolean success = parser.parseFile();
-//        assertFalse(success);
-//        System.out.println("");
-//
-//    }
+    /**
+     * tests to see if the parser will correctly handle an incorrect file
+     */
+    public void testSearchFail() throws Exception
+    {
+        String[] args = {"fail", "1", "2", "512"};
+        parser = new CommandParser(args);
+        boolean success = parser.parseFile();
+        assertFalse(success);
+        System.out.println("");
+
+    }
     
     /**
      * tests to see if the parser will correctly open and close a file
@@ -39,37 +39,37 @@ public class CommandParserTest extends TestCase
 
     }
 
-//    /**
-//     * tests checkDim for the variety of conditions that are available
-//     */
-//    public void testCheckDim() throws Exception
-//    {
-//        String[] args = {"test.txt", "1", "2", "512"};
-//
-//        parser = new CommandParser(args);
-//        assertTrue(parser.checkDim(0, 0, 1, 1));
-//        assertTrue(parser.checkDim(1, 1, 1, 1));
-//        assertFalse(parser.checkDim(0, 0, 1025, 1));
-//        assertFalse(parser.checkDim(0, 0, 1, 1025));
-//        assertFalse(parser.checkDim(-1, 1, 1, 1));
-//        assertFalse(parser.checkDim(0, -1, 1, 1));
-//        assertFalse(parser.checkDim(-1, -1, 1, 1));
-//        assertFalse(parser.checkDim(-1, -1, -1, 1));
-//        assertFalse(parser.checkDim(-1, -1, -1, -1));
-//        assertFalse(parser.checkDim(0,  0, 0, 0));
-//        assertFalse(parser.checkDim(0, 0, 1, 0));
-//        System.out.println("");
-//    }
+    /**
+     * tests checkDim for the variety of conditions that are available
+     */
+    public void testCheckDim() throws Exception
+    {
+        String[] args = {"test.txt", "1", "2", "512"};
+
+        parser = new CommandParser(args);
+        assertTrue(parser.checkDim(0, 0, 1, 1));
+        assertTrue(parser.checkDim(1, 1, 1, 1));
+        assertFalse(parser.checkDim(0, 0, 1025, 1));
+        assertFalse(parser.checkDim(0, 0, 1, 1025));
+        assertFalse(parser.checkDim(-1, 1, 1, 1));
+        assertFalse(parser.checkDim(0, -1, 1, 1));
+        assertFalse(parser.checkDim(-1, -1, 1, 1));
+        assertFalse(parser.checkDim(-1, -1, -1, 1));
+        assertFalse(parser.checkDim(-1, -1, -1, -1));
+        assertFalse(parser.checkDim(0,  0, 0, 0));
+        assertFalse(parser.checkDim(0, 0, 1, 0));
+        System.out.println("");
+    }
     
-//    /**
-//     * tests the parsers various tests
-//     */
-//    public void testParseFile() throws Exception
-//    {
-//        String[] args = {"test.txt", "1", "2", "512"};
-//        parser = new CommandParser(args);
-//        boolean success = parser.parseFile();
-//        assertTrue(success);
-//    }
+    /**
+     * tests the parsers various tests
+     */
+    public void testParseFile() throws Exception
+    {
+        String[] args = {"test.txt", "1", "2", "512"};
+        parser = new CommandParser(args);
+        boolean success = parser.parseFile();
+        assertTrue(success);
+    }
     
 }
