afa2c71bdf5991e04e8b99b7944894f5deef8560
jondef95@vt.edu <jondef95@github.com>
2016-04-29 12:42:34 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/SkipListTest.java changed at Fri Apr 29 12:42:34 EDT 2016

--- src/SkipListTest.java
+++ src/SkipListTest.java
@@ -36,161 +36,167 @@ public class SkipListTest extends TestCase
         list = new SkipList<String, Rectangle>();
     }
 
-//    /**
-//     * tests the insert method
-//     */
-//    public void testInsert() throws Exception
-//    {
-//        list.insert(pair1);
-//        list.dump();
-//        System.out.println("");
-//        list.insert(pair4);
-//        list.dump();
-//        System.out.println("");
-//
-//        assertNull(list.search("node3"));
-//        list.insert(pair2);
-//        list.dump();
-//        System.out.println("");
-//
-//        list.insert(pair3);
-//        assertEquals(pair3.compareTo(list.search("node3").getPair()),
-//            0);
-//
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node1");
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node2");
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node3");
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node4");
-//        list.dump();
-//
-//        list.insert(pair1);
-//        list.dump();
-//        System.out.println("");
-//        list.insert(pair4);
-//        list.dump();
-//        System.out.println("");
-//
-//        assertNull(list.search("node3"));
-//        list.insert(pair2);
-//        list.dump();
-//        System.out.println("");
-//
-//        list.insert(pair3);
-//        assertEquals(pair3.compareTo(list.search("node3").getPair()),
-//            0);
-//        list.dump();
-//        System.out.println("");
-//        
-//        list.insert(new KVPair<String, Rectangle>("node5", new Rectangle(
-//            "node5", 5, 5, 5, 5)));
-//        list.dump();
-//        System.out.println("");
-//        list.insert(new KVPair<String, Rectangle>("node6", new Rectangle(
-//            "node6", 6, 6, 6, 6)));
-//        list.dump();
-//        System.out.println("");
-//
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node1");
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node2");
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node3");
-//        list.dump();
-//        System.out.println("");
-//        list.removeKey("node4");
-//        list.dump();
-//        Manager.close();
-//    }
-    
+    // /**
+    // * tests the insert method
+    // */
+    // public void testInsert() throws Exception
+    // {
+    // list.insert(pair1);
+    // list.dump();
+    // System.out.println("");
+    // list.insert(pair4);
+    // list.dump();
+    // System.out.println("");
+    //
+    // assertNull(list.search("node3"));
+    // list.insert(pair2);
+    // list.dump();
+    // System.out.println("");
+    //
+    // list.insert(pair3);
+    // assertEquals(pair3.compareTo(list.search("node3").getPair()),
+    // 0);
+    //
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node1");
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node2");
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node3");
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node4");
+    // list.dump();
+    //
+    // list.insert(pair1);
+    // list.dump();
+    // System.out.println("");
+    // list.insert(pair4);
+    // list.dump();
+    // System.out.println("");
+    //
+    // assertNull(list.search("node3"));
+    // list.insert(pair2);
+    // list.dump();
+    // System.out.println("");
+    //
+    // list.insert(pair3);
+    // assertEquals(pair3.compareTo(list.search("node3").getPair()),
+    // 0);
+    // list.dump();
+    // System.out.println("");
+    //
+    // list.insert(new KVPair<String, Rectangle>("node5", new Rectangle(
+    // "node5", 5, 5, 5, 5)));
+    // list.dump();
+    // System.out.println("");
+    // list.insert(new KVPair<String, Rectangle>("node6", new Rectangle(
+    // "node6", 6, 6, 6, 6)));
+    // list.dump();
+    // System.out.println("");
+    //
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node1");
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node2");
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node3");
+    // list.dump();
+    // System.out.println("");
+    // list.removeKey("node4");
+    // list.dump();
+    // Manager.close();
+    // }
+
     public void testLoop() throws Exception
     {
         for (int i = 1; i < 20; i++)
         {
-            list.insert(new KVPair<String, Rectangle>(Integer.toString(i), new Rectangle(
-                Integer.toString(i), i, i, i, i)));
+            list.insert(new KVPair<String, Rectangle>(Integer
+                .toString(i), new Rectangle(Integer.toString(i), i, i,
+                    i, i)));
             list.dump();
             System.out.println("");
         }
         System.out.println("\n\n");
         for (int i = 1; i < 10; i++)
         {
-            list.removeKey(Integer.toString(i*2));
+            list.removeKey(Integer.toString(i * 2));
             list.dump();
             System.out.println("");
         }
         System.out.println("\n\n");
-        for (int i = 1; i < 20; i++)
+        for (int i = 1; i < i * 2; i++)
         {
-            list.insert(new KVPair<String, Rectangle>(Integer.toString(i), new Rectangle(
-                Integer.toString(i), i, i, i, i)));
+            list.insert(new KVPair<String, Rectangle>(Integer
+                .toString(i), new Rectangle(Integer.toString(i), i, i,
+                    i, i)));
             list.dump();
             System.out.println("");
         }
     }
-//
-//    /**
-//     * make sure that the list will adjust the head when a node is added higher
-//     * than the head
-//     */
-//    public void testRandomInsert() throws Exception
-//    {
-//        TestableRandom.setNextBooleans(false, true, false);
-//        assertEquals(1, list.getHead().next.length);
-//        assertTrue(list.insert(pair1));
-//        assertEquals(1, list.getHead().next.length);
-//        assertTrue(list.insert(pair2));
-//        assertEquals(2, list.getHead().next.length);
-//        list.dump();
-//    }
-//
-//    /**
-//     * creates a fake region to check for new rectangles
-//     */
-//    public void testRegionSearch() throws Exception
-//    {
-//        Rectangle region = new Rectangle("region", 100, 100, 200,
-//            200);
-//        SkipList<String, Rectangle> regionList = new SkipList<String, Rectangle>();
-//        regionList.insert(new KVPair<String, Rectangle>(
-//            "notIntersect", new Rectangle("notIntersect", 10, 10, 20,
-//                20)));
-//        assertFalse(regionList.regionSearch(region));
-//        regionList.insert(new KVPair<String, Rectangle>("intersect1",
-//            new Rectangle("intersect1", 75, 75, 200, 250)));
-//        assertTrue(regionList.regionSearch(region));
-//    }
-//
-//    /**
-//     * tests that the intersections test successfully finds an intersection and
-//     * returns the appropriate boolean
-//     */
-//    public void testIntersections() throws Exception
-//    {
-//        SkipList<String, Rectangle> intersectList = new SkipList<String, Rectangle>();
-//        intersectList.insert(new KVPair<String, Rectangle>(
-//            "notIntersect", new Rectangle("notIntersect", 10, 10, 20,
-//                20)));
-//        assertFalse(intersectList.intersections());
-//        intersectList.insert(new KVPair<String, Rectangle>(
-//            "intersect1", new Rectangle("intersect1", 75, 75, 200,
-//                250)));
-//        assertFalse(intersectList.intersections());
-//        intersectList.insert(new KVPair<String, Rectangle>(
-//            "intersect2", new Rectangle("intersect2", 100, 100, 200,
-//                200)));
-//        assertTrue(intersectList.intersections());
-//
-//    }
+    //
+    // /**
+    // * make sure that the list will adjust the head when a node is added
+    // higher
+    // * than the head
+    // */
+    // public void testRandomInsert() throws Exception
+    // {
+    // TestableRandom.setNextBooleans(false, true, false);
+    // assertEquals(1, list.getHead().next.length);
+    // assertTrue(list.insert(pair1));
+    // assertEquals(1, list.getHead().next.length);
+    // assertTrue(list.insert(pair2));
+    // assertEquals(2, list.getHead().next.length);
+    // list.dump();
+    // }
+    //
+    // /**
+    // * creates a fake region to check for new rectangles
+    // */
+    // public void testRegionSearch() throws Exception
+    // {
+    // Rectangle region = new Rectangle("region", 100, 100, 200,
+    // 200);
+    // SkipList<String, Rectangle> regionList = new SkipList<String,
+    // Rectangle>();
+    // regionList.insert(new KVPair<String, Rectangle>(
+    // "notIntersect", new Rectangle("notIntersect", 10, 10, 20,
+    // 20)));
+    // assertFalse(regionList.regionSearch(region));
+    // regionList.insert(new KVPair<String, Rectangle>("intersect1",
+    // new Rectangle("intersect1", 75, 75, 200, 250)));
+    // assertTrue(regionList.regionSearch(region));
+    // }
+    //
+    // /**
+    // * tests that the intersections test successfully finds an intersection
+    // and
+    // * returns the appropriate boolean
+    // */
+    // public void testIntersections() throws Exception
+    // {
+    // SkipList<String, Rectangle> intersectList = new SkipList<String,
+    // Rectangle>();
+    // intersectList.insert(new KVPair<String, Rectangle>(
+    // "notIntersect", new Rectangle("notIntersect", 10, 10, 20,
+    // 20)));
+    // assertFalse(intersectList.intersections());
+    // intersectList.insert(new KVPair<String, Rectangle>(
+    // "intersect1", new Rectangle("intersect1", 75, 75, 200,
+    // 250)));
+    // assertFalse(intersectList.intersections());
+    // intersectList.insert(new KVPair<String, Rectangle>(
+    // "intersect2", new Rectangle("intersect2", 100, 100, 200,
+    // 200)));
+    // assertTrue(intersectList.intersections());
+    //
+    // }
 }
