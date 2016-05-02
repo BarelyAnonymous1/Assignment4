7ffdd5a1302c18f54f3c79f1fac5ff62c79bbcbc
jondef95@vt.edu <jondef95@github.com>
2016-05-01 22:39:50 -0400

    File /C:/Users/Jonathan/git/Assignment4/src/FreeList.java changed at Sun May 01 22:39:50 EDT 2016

--- src/FreeList.java
+++ src/FreeList.java
@@ -192,7 +192,6 @@ public class FreeList
             {
                 curr.length += curr.next.length;
                 remove(curr.next.index);
-                size--;
             }
             curr = curr.next;
         }
