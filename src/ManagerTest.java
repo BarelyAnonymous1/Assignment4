aaa2fafe971def4ec385dcb188605344b6604f8e
Preston Lattimer <preston.lattimer@gmail.com>
2016-05-02 21:26:51 -0400

    File /Users/prestonlattimer/Documents/workspace/Assignment4/src/ManagerTest.java changed at Mon May 02 21:26:50 EDT 2016

---
+++ src/ManagerTest.java
@@ -0,0 +1,22 @@
+import student.TestCase;
+
+
+/**
+ * @author prestonlattimer
+ *
+ */
+public class ManagerTest extends TestCase {
+
+    private Manager manager;
+    
+    public void setUp()
+    {
+        manager = new Manager();
+    }
+    
+    public void testManager()
+    {
+        manager.equals(new Object());
+    }
+    
+}
